cff1523cd7d6a92a61ce0da00345696ce2b022a0
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-10-09 15:38:24 +0530

    setup react

---
+++ src/components/userManagement.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function userManagement() {
+  return (
+    <>
+    <div>
+        <h1>User Management</h1>
+    </div>
+    </>
+  )
+}
+
+export default userManagement
