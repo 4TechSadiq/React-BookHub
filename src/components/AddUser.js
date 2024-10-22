e34636582ed9e1f2a722a590fb095246b27681ce
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-10-22 23:51:41 +0530

    Update AddUser component to include new user form

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,14 +1,30 @@
 import React from 'react'
 
 function AddUser() {
-  return (
+return (
     <>
-    <div className='container mt-4'>
-        <h2 className='text-center'>New User</h2>
-
-    </div>
+        <div className='container mt-4 d-flex justify-content-center'>
+            <div className='col-6 shadow rounded-5 p-5'>
+                <h2 className='text-center'>New User</h2>
+                <form>
+                    <div className='mb-3'>
+                        <label htmlFor='userID' className='form-label'>User ID</label>
+                        <input type='text' className='form-control' id='userID'></input>
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='userName' className='form-label'>User Name</label>
+                        <input type='text' className='form-control' id='userName'></input>
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='institution' className='form-label'>Institution</label>
+                        <input type='text' className='form-control' id='institution'></input>
+                    </div>
+                    <button className='btn btn-success'>Submit</button>
+                </form>
+            </div>
+        </div>
     </>
-  )
+)
 }
 
 export default AddUser
