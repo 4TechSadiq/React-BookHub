7a32d4538213f9a4a45c1cd444ba6957807aadd3
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-10-22 23:20:15 +0530

    Update Catelogs component with new categories

--- src/components/Catelogs.js
+++ src/components/Catelogs.js
@@ -9,7 +9,7 @@ function Catelogs() {
                 <div className="card mt-5 shadow" style={{ width: '18rem' }}>
                     <img src="path_to_image" className="card-img-top" alt="Card image" />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title">Thrillers</h5>
                         <p className="card-text">
                         Some quick example text to build on the card title and make up the bulk of the card's content.
                         </p>
@@ -19,7 +19,7 @@ function Catelogs() {
                 <div className="card mt-5 shadow" style={{ width: '18rem' }}>
                     <img src="path_to_image" className="card-img-top" alt="Card image" />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title">Novels</h5>
                         <p className="card-text">
                         Some quick example text to build on the card title and make up the bulk of the card's content.
                         </p>
@@ -29,7 +29,7 @@ function Catelogs() {
                 <div className="card mt-5 shadow" style={{ width: '18rem' }}>
                     <img src="path_to_image" className="card-img-top" alt="Card image" />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title">Self Motives</h5>
                         <p className="card-text">
                         Some quick example text to build on the card title and make up the bulk of the card's content.
                         </p>
